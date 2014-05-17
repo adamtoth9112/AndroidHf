71bd485a63795d5e0581b603ce426fe119a20c7a
adamtoth <adamtoth9112@gmail.com>
2014-05-17 16:24:55 +0200

    aszinkron zene

--- Hit'NSync/src/hu/lilacode/hitnsync/service/music/GameMusic.java
+++ Hit'NSync/src/hu/lilacode/hitnsync/service/music/GameMusic.java
@@ -2,6 +2,7 @@ package hu.lilacode.hitnsync.service.music;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 
 public class GameMusic {
 MediaPlayer mediaPlayer;
@@ -12,14 +13,20 @@ MediaPlayer mediaPlayer;
 		mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		try {
 			mediaPlayer.setDataSource(url);
-			mediaPlayer.prepare();
+			mediaPlayer.prepareAsync();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
 		
 		
 		mediaPlayer.setLooping(true);
-		mediaPlayer.start();
+		mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
+			
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				mediaPlayer.start();
+			}
+		});
 	}
 	
 	public void play() {
