dc345c710dd2247895a608794e91738d36d5fdb8
Kope91 <kolocz.peter@gmail.com>
2014-05-17 16:38:50 +0200

    2kép

--- Hit'NSync/gen/hu/lilacode/hitnsync/R.java
+++ Hit'NSync/gen/hu/lilacode/hitnsync/R.java
@@ -25,8 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int main=0x7f020001;
         public static final int ocean=0x7f020002;
-        public static final int splash=0x7f020003;
-        public static final int splash_black=0x7f020004;
+        public static final int option=0x7f020003;
+        public static final int splash=0x7f020004;
+        public static final int splash_black=0x7f020005;
+        public static final int stats=0x7f020006;
     }
     public static final class id {
         public static final int NameField=0x7f090011;
--- Hit'NSync/src/hu/lilacode/hitnsync/menu/Stats.java
+++ Hit'NSync/src/hu/lilacode/hitnsync/menu/Stats.java
@@ -8,12 +8,10 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Stats extends Activity {
-	PlayerStats ps;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.stats);
-		ps = new PlayerStats();
 		
 		View table = (View) findViewById(R.id.tableRow1);
 		View row1 = (View) findViewById(R.id.tableRow2);
@@ -23,7 +21,7 @@ public class Stats extends Activity {
 		TextView r1f3 = (TextView) findViewById(R.id.textView3);
 		TextView r1f4 = (TextView) findViewById(R.id.textView4);
 		
-		r1f1.setText(ps.stats.get(0).name.toString());
+		
 
 	}
 
